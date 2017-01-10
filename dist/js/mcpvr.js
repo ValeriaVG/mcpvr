3bfb7c108d036368b3fd83c19a6da447cd288c0e
ValeriaVG <valeria.viana.gusmao@gmail.com>
2017-01-11 02:20:09 +0300

    columns fix

--- dist/js/mcpvr.js
+++ dist/js/mcpvr.js
@@ -1,4 +1,4 @@
-/*! MCPVR - v0.0.1 - 2017-01-09
+/*! MCPVR - v0.0.1 - 2017-01-11
 * https://valeriavg.github.io/mcpvr/
 * Copyright (c) 2017 ValeriaVG <valeria.viana.gusmao@gmail.com>; Licensed GPL-3.0 */
 var makeColumnsEqual = function(selector) {
